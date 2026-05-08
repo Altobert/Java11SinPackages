d4b966c6c939785c434457feaca5b8764a2df050
Alberto San Martin Concha <alberto.sanmartin@sii.cl>
2026-05-08 14:53:05 -0400

    inicio java ocp 8

--- src/cat/BigCat.java
+++ src/cat/BigCat.java
@@ -1,10 +1,10 @@
 package cat;
 
 public class BigCat{
-    public String name;
-    protected boolean hasFur;
-    boolean hasPaws;
-    private int id;
+    public String name; // publica se accede desde cualquier clase
+    protected boolean hasFur; // protegida se accede desde la misma clase y subclases
+    boolean hasPaws;            // default se accede desde el mismo paquete
+    private int id;             // privada se accede solo desde la misma clase
 
     public static void main(String[] args) {
         BigCat bigCat = new BigCat();
@@ -12,6 +12,7 @@ public class BigCat{
         bigCat.hasFur = true;
         bigCat.hasPaws = true;
         bigCat.id = 1; // This line will cause a compilation error because 'id' is private
+        System.out.println(bigCat.id); // This line will also cause a compilation error because 'id' is private
         System.out.println("BigCat name: " + bigCat.name + ", hasFur: " + bigCat.hasFur + ", hasPaws: " + bigCat.hasPaws);
     }
 }
---
+++ src/cat/Principal.java
@@ -0,0 +1,15 @@
+package cat;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        BigCat bigCat = new BigCat();
+        bigCat.name = "Leopard";
+        bigCat.hasFur = true;
+        bigCat.hasPaws = true;
+        bigCat.id = 1; // This line will cause a compilation error because 'id' is private
+        System.out.println(id); // This line will also cause a compilation error because 'id' is private
+        System.out.println("BigCat name: " + bigCat.name + ", hasFur: " + bigCat.hasFur + ", hasPaws: " + bigCat.hasPaws);
+    }
+
+}
