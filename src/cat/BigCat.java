74f28d3c0e56ff23a2e626fedf8111409924802f
Alberto San Martin Concha <alberto.sanmartin@sii.cl>
2026-05-08 15:40:23 -0400

    pruebas compilacion clases

--- src/cat/BigCat.java
+++ src/cat/BigCat.java
@@ -4,6 +4,6 @@ public class BigCat{
     public String name;         // publica se accede desde cualquier clase
     protected boolean hasFur;   // protegida se accede desde la misma clase y subclases
     boolean hasPaws;            // default se accede desde el mismo paquete
-    public int id;           // privada se accede solo desde la misma clase    
+    protected int id;           //
 }
     
--- src/cat/species/Lynx.java
+++ src/cat/species/Lynx.java
@@ -1,5 +1,8 @@
 
 package cat.species;
 
+import cat.*;
+
 // extends y no extend
+// debido a que no me encuentro en el mismo pacuete, debo importar
 public class Lynx extends BigCat{}
