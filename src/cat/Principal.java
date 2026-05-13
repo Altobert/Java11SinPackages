263498e687ff4a6a015519600e2d7054b9665325
Alberto San Martin Concha <alberto.sanmartin@sii.cl>
2026-05-13 11:36:41 -0400

    inicio java ocp 8

--- src/cat/Principal.java
+++ src/cat/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
     public static void main(String[] args) {
         BigCat bigCat = new BigCat();
         bigCat.name = "Leopard";
-        bigCat.hasFur = true;
+        bigCat.hasFur = true;      // 
         bigCat.hasPaws = true;
         bigCat.id = 1; // This line will cause a compilation error because 'id' is private
         System.out.println(bigCat.id); // This line will also cause a compilation error because 'id' is private
